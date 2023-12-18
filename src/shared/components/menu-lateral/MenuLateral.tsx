508c5e9f215cf087a9df272e48fbb5c841147bd2
Artur Moisés Alves de Paula <arturmoiscontato@gmail.com>
2023-12-17 21:38:18 -0300

    reactor: add menu options on drawer

--- src/shared/components/menu-lateral/MenuLateral.tsx
+++ src/shared/components/menu-lateral/MenuLateral.tsx
@@ -1,5 +1,37 @@
 import { Avatar, Box, Divider, Drawer, Icon, List, ListItemButton, ListItemIcon, ListItemText, useMediaQuery, useTheme } from '@mui/material';
 import { useDrawerContext } from '../../contexts';
+import { useMatch, useNavigate, useResolvedPath } from 'react-router-dom';
+
+interface IListItemLinkProps {
+  to: string
+  label: string
+  icon: string
+  onClick?: () => void
+}
+
+const ListItemLink: React.FC<IListItemLinkProps> = ({ to, label, icon, onClick }) => {
+  const navigate = useNavigate();
+
+  const resolvedPath = useResolvedPath(to);
+  const match = useMatch({
+    path: resolvedPath.pathname,
+    end: false
+  });
+
+
+  const handleClick = () => {
+    navigate(to);
+    onClick?.();
+  };
+  return (
+    <ListItemButton selected={!!match} onClick={handleClick}>
+      <ListItemIcon>
+        <Icon>{icon}</Icon>
+      </ListItemIcon>
+      <ListItemText primary={label} />
+    </ListItemButton>
+  );
+};
 
 interface IMenuLateralProps {
   children: React.ReactNode
@@ -7,14 +39,15 @@ interface IMenuLateralProps {
 export const MenuLateral: React.FC<IMenuLateralProps> = ({ children }) => {
   const theme = useTheme();
   const smDown = useMediaQuery(theme.breakpoints.down('sm'));
-  
-  const {isDrawerOpen, toggleDrawerOpen} = useDrawerContext();
+
+  const { isDrawerOpen, toggleDrawerOpen, drawerOptions } = useDrawerContext();
   console.log(isDrawerOpen);
 
-  return (  
+  return (
     <>
-      <Drawer open={isDrawerOpen} variant={smDown ? 'temporary' :'permanent'} onClose={toggleDrawerOpen}>
+      <Drawer open={isDrawerOpen} variant={smDown ? 'temporary' : 'permanent'} onClose={toggleDrawerOpen}>
         <Box width={theme.spacing(28)} height='100%' display='flex' flexDirection='column'>
+
           <Box width='100%' height={theme.spacing(20)} display='flex' alignItems='center' justifyContent='center'>
             <Avatar
               sx={{
@@ -24,19 +57,25 @@ export const MenuLateral: React.FC<IMenuLateralProps> = ({ children }) => {
               alt='Remy Sharp'
               src='https://media.licdn.com/dms/image/D4D03AQHIiMX4i4UXvw/profile-displayphoto-shrink_800_800/0/1699879557886?e=1708560000&v=beta&t=C-Nk65WkVNrHDNcLhH8hoKG_NJw35QCgdQXes61S9UM' />
           </Box>
+
           <Divider />
+
           <Box flex={1}>
             <List component='nav'>
-              <ListItemButton>
-                <ListItemIcon>
-                  <Icon>home</Icon>
-                </ListItemIcon>
-                <ListItemText primary='Página inicial'/>
-              </ListItemButton>
+              {drawerOptions.map(drawerOptions => (
+                <ListItemLink
+                  key={drawerOptions.path}
+                  icon={drawerOptions.icon}
+                  label={drawerOptions.label}
+                  to={drawerOptions.path}
+                  onClick={smDown ? toggleDrawerOpen : undefined}
+                />
+              ))}
             </List>
           </Box>
         </Box>
       </Drawer >
+
       <Box height='100vh' marginLeft={smDown ? 0 : theme.spacing(28)}>
         {children}
       </Box>
