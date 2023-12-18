891988f9f4d51d8cde365acd76d6b4d81fdf65a0
Artur Moisés Alves de Paula <arturmoiscontato@gmail.com>
2023-12-18 09:55:45 -0300

    feat: create base layout for pages

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,10 @@
-import { Button } from '@mui/material';
 import { Routes, Route } from 'react-router-dom';
 import { useDrawerContext } from '../shared/contexts';
 import { useEffect } from 'react';
+import { Dashboard } from '../pages';
 
 export const AppRoutes = () => {
-  const { toggleDrawerOpen, setDrawerOptions } = useDrawerContext();
+  const { setDrawerOptions } = useDrawerContext();
 
   useEffect(() => {
     setDrawerOptions([
@@ -20,11 +20,7 @@ export const AppRoutes = () => {
     <Routes>
       <Route
         path="/pagina-inicial"
-        element={<Button
-          variant='contained'
-          color='primary'
-          onClick={toggleDrawerOpen}>Toggle Drawer</Button>
-        } />
+        element={<Dashboard />} />
       {/* <Route path="*" element={<Navigate to='/pagina-inicial' />} /> */}
     </Routes>
   );
